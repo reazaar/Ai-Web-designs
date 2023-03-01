e163d9bff12680992877a0ff2c8131edcef4465e
reazaar <125664288+reazaar@users.noreply.github.com>
2023-03-01 15:12:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var Typed = new Typed(".multiple-text", {
+	strings: ["Web-Designer", "Graghic-Designer", "Visual-Engineer", "Light-Engineer", "Sound Technition", "Video-Editor", "3D-Mapping", "Stage-Designer", "Camera-man", "Cricket-Coach"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+});
+
+
